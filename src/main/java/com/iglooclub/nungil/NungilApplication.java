2f86a9ecb6c3b4264bdef209eff4dcd6da7c3f3b
clap_0 <clap-0@naver.com>
2024-01-17 14:29:58 +0900

    init project

---
+++ src/main/java/com/iglooclub/nungil/NungilApplication.java
@@ -0,0 +1,13 @@
+package com.iglooclub.nungil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NungilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NungilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iglooclub/nungil/NungilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iglooclub.nungil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NungilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
