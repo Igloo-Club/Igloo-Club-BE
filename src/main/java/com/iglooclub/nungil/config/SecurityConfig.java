16f9e7aaad4e59c5b107c9120a22e9256e94f505
clap_0 <clap-0@naver.com>
2024-01-23 13:00:52 +0900

    feat: 스프링 시큐리티 설정 파일 추가

---
+++ src/main/java/com/iglooclub/nungil/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.iglooclub.nungil.config;
+
+import com.iglooclub.nungil.config.jwt.JwtAccessDeniedHandler;
+import com.iglooclub.nungil.config.jwt.JwtAuthenticationEntryPoint;
+import com.iglooclub.nungil.config.jwt.TokenAuthenticationFilter;
+import com.iglooclub.nungil.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    /**
+     * 스프링 시큐리티 기능 비활성화
+     */
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring()
+                // 정적 리소스에 대한 스프링 시큐리티 사용을 비활성화
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    /**
+     * 특정 HTTP 요청에 대한 웹 기반 보안 구성
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                // 토큰 기반 인증을 사용하기 때문에 세션 기능 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                // 인증, 인가 설정
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                // 헤더를 확인할 커스텀 필터 추가
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+
+                // 인증 및 인가 예외 처리
+                .exceptionHandling()
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                .accessDeniedHandler(new JwtAccessDeniedHandler());
+
+        return http.build();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+}
+
