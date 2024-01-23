603dd9363e2e09045d615861f77c59460e09a149
clap_0 <clap-0@naver.com>
2024-01-23 12:51:45 +0900

    feat: TokenProvider 추가

---
+++ src/main/java/com/iglooclub/nungil/config/jwt/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.iglooclub.nungil.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String header;
+    private String secret;
+}
