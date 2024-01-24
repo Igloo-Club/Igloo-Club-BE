0519700c4cb16b64b022df6264ba1bd05d365d4c
clap_0 <clap-0@naver.com>
2024-01-25 04:55:31 +0900

    fix: RuntimeException에 메시지를 적용하도록 수정

--- src/main/java/com/iglooclub/nungil/exception/GeneralException.java
+++ src/main/java/com/iglooclub/nungil/exception/GeneralException.java
@@ -1,11 +1,14 @@
 package com.iglooclub.nungil.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class GeneralException extends RuntimeException implements CustomException {
 
     private final ErrorResult errorResult;
+
+    public GeneralException(ErrorResult errorResult) {
+        super(errorResult.getMessage());
+        this.errorResult = errorResult;
+    }
 }
