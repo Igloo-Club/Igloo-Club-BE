b7c080b3f6cf02025e91cd85a430fd0659d3f3ef
clap_0 <clap-0@naver.com>
2024-01-29 12:47:19 +0900

    feat: 요일에 관한 Enum 클래스 추가

---
+++ src/main/java/com/iglooclub/nungil/domain/enums/yoil.java
@@ -0,0 +1,19 @@
+package com.iglooclub.nungil.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum yoil {
+    SUN("일요일"),
+    MON("월요일"),
+    TUE("화요일"),
+    WED("수요일"),
+    THU("목요일"),
+    FRI("금요일"),
+    SAT("토요일"),
+    ;
+
+    private final String title;
+}
