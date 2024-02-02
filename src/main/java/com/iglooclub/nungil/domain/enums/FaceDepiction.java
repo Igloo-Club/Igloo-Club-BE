6d9b24d7bd9f94096df250c412f8ca751d9c7e4a
Seunghan Ryu <jorzor99@gmail.com>
2024-02-02 12:50:55 +0900

    fix : face, personality enum

--- src/main/java/com/iglooclub/nungil/domain/enums/FaceDepiction.java
+++ src/main/java/com/iglooclub/nungil/domain/enums/FaceDepiction.java
@@ -9,7 +9,26 @@ public enum FaceDepiction {
     PRETTY_EYES("예쁜 눈"),
     BOLD_EYEBROWS("짙은 눈썹"),
     ATTRACTIVE_VOICE("매력적인 목소리"),
-    ;
+    TALL("키가 커요"),
+    PETITE("키가 아담해요"),
+    SLIM("슬림해요"),
+    MUSCULAR("근육질 몸매에요"),
+    SLIGHTLY_PLUMP("약간 통통해요"),
+    BROAD_SHOULDERS("어깨가 넓어요"),
+    STRONG_LOWER_BODY("하체가 튼튼해요"),
+    LONG_ARMS("팔길이가 길어요"),
+    LONG_LEGS("다리가 길어요"),
+    SOFT_FIGURE("체형이 부드럽게 보어요"),
+    BRIGHT_EYES("눈이 크고 맑아요"),
+    EYE_SMILE("눈웃음이 이뻐요"),
+    SHARP_NOSE("코가 오똑해요"),
+    CLEAN_JAWLINE("턱선이 깔끔해요"),
+    CUTE_CHEEKS("볼살이 귀여워요"),
+    ANGULAR_FACE("얼굴형이 각진 편이에요"),
+    ROUND_FACE("얼굴형이 둥근 편이에요"),
+    BRIGHT_SKIN("피부가 밝아요"),
+    CLEAN_TEETH("이가 깔끔해요"),
+    EXOTIC_LOOKS("외모가 이국적이에요");
 
     private final String title;
 }
--- src/main/java/com/iglooclub/nungil/domain/enums/PersonalityDepiction.java
+++ src/main/java/com/iglooclub/nungil/domain/enums/PersonalityDepiction.java
@@ -8,7 +8,20 @@ import lombok.RequiredArgsConstructor;
 public enum PersonalityDepiction {
     POLITE("예의가 발라요"),
     EXPRESSIVE("표현을 잘해요"),
-    ;
+    ACTIVE("활발해요"),
+    SOCIABLE("사교적이에요"),
+    CALM("차분해요"),
+    KIND("친절해요"),
+    CONSIDERATE("배려를 잘해요"),
+    GOOD_SENSE_OF_HUMOR("유머 감각이 좋아요"),
+    DILIGENT("성실해요"),
+    METICULOUS("꼼꼼해요"),
+    RESPONSIBLE("책임감이 강해요"),
+    OPTIMISTIC("낙천적이에요"),
+    EMPATHETIC("공감능력이 뛰어나요"),
+    PASSIONATE("열정적이에요"),
+    WELL_PLANNED("계획적이에요"),
+    GOOD_AT_EXPRESSING_FEELINGS("감정 표현을 잘해요");
 
     private final String title;
 }
