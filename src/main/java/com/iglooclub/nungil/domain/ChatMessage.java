a4ed1a2f6cb2510376dc64e9423c781faf1609e7
clap_0 <clap-0@naver.com>
2024-02-02 18:01:55 +0900

    fix: 채팅 메시지 엔티티에 메시지 상태에 관한 필드 추가

--- src/main/java/com/iglooclub/nungil/domain/ChatMessage.java
+++ src/main/java/com/iglooclub/nungil/domain/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.iglooclub.nungil.domain;
 
+import com.iglooclub.nungil.domain.enums.ChatMessageStatus;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,6 +29,10 @@ public class ChatMessage {
 
     @Column(length = 400)
     private String content;
+
+    // 개발의 편의를 위해 일단 기본값을 READ로 설정. 추후 읽지 않은 메시지 개발 시 UNREAD로 변경
+    @Column(name = "status")
+    private ChatMessageStatus status = ChatMessageStatus.READ;
 
     private LocalDateTime createdAt;
 }
