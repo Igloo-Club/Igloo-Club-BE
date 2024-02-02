03bf1a388fc6fdb04038fa8718f3eeb23320d173
clap_0 <clap-0@naver.com>
2024-02-02 17:25:37 +0900

    feat: 채팅방 및 채팅 메시지에 관한 엔티티 추가

---
+++ src/main/java/com/iglooclub/nungil/domain/ChatMessage.java
@@ -0,0 +1,33 @@
+package com.iglooclub.nungil.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(length = 400)
+    private String content;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/iglooclub/nungil/domain/ChatRoom.java
@@ -0,0 +1,35 @@
+package com.iglooclub.nungil.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Member receiver;
+
+    @OneToMany(mappedBy = "chatRoom")
+    List<ChatMessage> chatMessageList = new ArrayList();
+
+    private LocalDateTime createdAt;
+}
