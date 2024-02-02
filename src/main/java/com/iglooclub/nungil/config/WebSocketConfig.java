d6b84c9a628d226d3ddc51f0d7fc4d5bb39047c2
clap_0 <clap-0@naver.com>
2024-02-02 17:25:00 +0900

    feat: 웹 소켓 및 STOMP에 관한 설정 파일 추가

---
+++ src/main/java/com/iglooclub/nungil/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.iglooclub.nungil.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * 웹 소켓 설정 클래스입니다.
+ * WebSocket과 Stomp 프로토콜을 이용한 메시징을 활성화합니다.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/stomp")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/chat");
+    }
+}
