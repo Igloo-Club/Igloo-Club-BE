5527914e58f0a86c3ae7fffc290fbebe949bfb8a
clap_0 <clap-0@naver.com>
2024-02-07 19:00:15 +0900

    fix: 웹 소켓 요청 인증을 소켓 연결 시에만 수행하도록 수정

--- src/main/java/com/iglooclub/nungil/config/jwt/ChatPreHandler.java
+++ src/main/java/com/iglooclub/nungil/config/jwt/ChatPreHandler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.core.Authentication;
@@ -22,17 +23,20 @@ public class ChatPreHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
 
-        // 요청 헤더의 Authorization 키의 값 조회
-        String authorizationHeader = String.valueOf(headerAccessor.getNativeHeader(HEADER_AUTHORIZATION));
-        // 가져온 값에서 접두사 제거
-        String token = getAccessToken(authorizationHeader);
-        // 가져온 토큰이 유효한지 확인하고, 유효한 때는 인증 정보 설정
-        if (tokenProvider.validateToken(token)) {
-            Authentication authentication = tokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        } else {
-            // ChatErrorHandler 클래스에서 처리하는 예외와 에러 메시지가 "UNAUTHORIZED"로 동일해야 한다.
-            throw new MessageDeliveryException("UNAUTHORIZED");
+        if (StompCommand.CONNECT == headerAccessor.getCommand()) {
+            // 요청 헤더의 Authorization 키의 값 조회
+            String authorizationHeader = String.valueOf(headerAccessor.getFirstNativeHeader(HEADER_AUTHORIZATION));
+
+            // 가져온 값에서 접두사 제거
+            String token = getAccessToken(authorizationHeader);
+            // 가져온 토큰이 유효한지 확인하고, 유효한 때는 인증 정보 설정
+            if (tokenProvider.validateToken(token)) {
+                Authentication authentication = tokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                // ChatErrorHandler 클래스에서 처리하는 예외와 에러 메시지가 "UNAUTHORIZED"로 동일해야 한다.
+                throw new MessageDeliveryException("UNAUTHORIZED");
+            }
         }
 
         return message;
