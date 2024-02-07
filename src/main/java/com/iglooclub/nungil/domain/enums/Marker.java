9f063849949fe9c9916ab92d59e3a7e8fad34ecb
clap_0 <clap-0@naver.com>
2024-02-07 21:25:42 +0900

    feat: Marker Enum 클래스에 좌표 및 지역 필드 추가

--- src/main/java/com/iglooclub/nungil/domain/enums/Marker.java
+++ src/main/java/com/iglooclub/nungil/domain/enums/Marker.java
@@ -7,6 +7,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Marker {
 
+    //=== 광화문 ===//
+    GWANGHWAMUN_SQUARE("광화문 광장", 37.572776, 126.976890, Location.GWANGHWAMUN),
+
+    //=== 판교 ===//
+    PANGYO_STATION_SQUARE("판교역 광장", 37.39525750009229, 127.11148651523494, Location.PANGYO),
     ;
+
     private final String title;
+    private final Double latitude;
+    private final Double longitude;
+    private final Location location;
 }
