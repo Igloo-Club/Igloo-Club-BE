5e18cc11cf402aa653ffda78ab5ef8f220ccfc89
Seunghan Ryu <jorzor99@gmail.com>
2024-03-24 19:09:12 +0900

    fix : apply LoD(getSenderPhoneNumber, getMatcherPhoneNumber)

--- src/main/java/com/iglooclub/nungil/eventListener/NungilEventListener.java
+++ src/main/java/com/iglooclub/nungil/eventListener/NungilEventListener.java
@@ -4,7 +4,6 @@ import com.iglooclub.nungil.domain.events.NungilMatchedEvent;
 import com.iglooclub.nungil.domain.events.NungilSentEvent;
 import com.iglooclub.nungil.util.CoolSMS;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -21,7 +20,7 @@ public class NungilEventListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void matchNungilListen(NungilMatchedEvent nungilMatchedEvent){
         // 눈길 보낸 사용자에게 알림 전송
-        String phoneNumber = nungilMatchedEvent.getSender().getPhoneNumber();
+        String phoneNumber = getMatcherPhoneNumber(nungilMatchedEvent);
         String url = BASE_URL + "/finishmatch/" + nungilMatchedEvent.getSentNungil().getId();
         String text = "[눈길] 축하해요! 서로의 눈길이 닿았어요. 채팅방을 통해 두 분의 첫만남 약속을 잡아보세요.\n" + url;
 
@@ -29,14 +28,20 @@ public class NungilEventListener {
     }
 
     @Async
-    @EventListener
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void sentNungilListen(NungilSentEvent nungilSentEvent){
         // 눈길 보낸 사용자에게 알림 전송
-        String phoneNumber = nungilSentEvent.getSender().getPhoneNumber();
+        String phoneNumber = getSenderPhoneNumber(nungilSentEvent);
         String url = BASE_URL + "/receiveddetailpage/" + nungilSentEvent.getSentNungil().getId();
         String text = "[눈길] 새로운 눈길이 도착했어요. 얼른 확인해보세요!\n" + url;
 
         coolSMS.send(phoneNumber, text);
+    }
+
+    static final private String getSenderPhoneNumber(NungilSentEvent nungilSentEvent){
+        return nungilSentEvent.getSender().getPhoneNumber();
+    }
+    static final private String getMatcherPhoneNumber(NungilMatchedEvent nungilMatchedEvent){
+        return nungilMatchedEvent.getSender().getPhoneNumber();
     }
 }
